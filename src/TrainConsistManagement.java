05bf7628859024449b3cc441d08e16978b53d656
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-03-25 10:16:55 +0530

    Add Train Consist Management application
    Implemented a Train Consist Management application that tracks unique bogie IDs using a HashSet.

--- src/TrainConsistManagement.java
+++ src/TrainConsistManagement.java
@@ -1,1 +1,17 @@
-
+import java.util.HashSet;
+public class TrainConsistManagement {
+    public static void main(String[] args) {
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("UC3: Track Unique Bogie IDs");
+        HashSet<String> bogieIds = new HashSet<>();
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        bogieIds.add("BG101"); 
+        bogieIds.add("BG104");
+        bogieIds.add("BG102");
+        System.out.println("Final bogie IDs in the train:");
+        System.out.println(bogieIds);
+        System.out.println("Total unique bogies: " + bogieIds.size());
+    }
+}
